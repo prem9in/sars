4a8c2e81afff83d7c806169294d73cf1817ed0ca
Prem Prakash <v.premp@gmail.com>
2018-12-15 09:28:57 -0800

    api endpoint

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,7 @@
 const settings = {
 	bingmapsApiUrl: "https://dev.virtualearth.net/REST/v1/Locations/{{point}}?includeEntityTypes=Address&key={{key}}",
 	bingmapsApiKey: "AqwSSw6YyV-n2OjGf8nHvNHCprXR8YFGWhQPaXq4gXjZZ7Al-3xMaqY3VoXuK0VS",
-	searchApiUrl: "http://localhost:5000",
+	searchApiUrl: "http://13.77.179.28",
 	searchUrlTemplate: "{{endpoint}}/v1/search?text={{searchtext}}&city={{searchcity}}&state={{searchstate}}"
 };
 
